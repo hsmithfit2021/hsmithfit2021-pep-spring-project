9f85ecc746d9d0ba7621cbb78ec415bddeaa0f3c
Hunter Smith <hsmith2021@my.fit.edu>
2025-04-24 18:55:02 +0000

    Incomplete Formatted Repositories and Services

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,23 @@
 package com.example.service;
 
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+    @Autowired
+    public AccountService(AccountRepository ar) {
+        this.accountRepository = ar;
+    }
+
+    public Account login(Account a) {
+        return null;
+    }
+    public Account register(Account a) {
+        return null;
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,36 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.repository.MessageRepository;
+
+import com.example.entity.Message;
+
 public class MessageService {
+    MessageRepository messageRepository;
+    @Autowired
+    public MessageService(MessageRepository mr) {
+        this.messageRepository = mr;
+    }
+
+    public Message createNewMessage(Message m) {
+        return null;
+    }
+    public List<Message> getAllMessages() {
+        return null;
+    }
+    public Message getMessageWithId(int id) {
+        return null;
+    }
+    public Message deleteMessageWithId(int id) {
+        return null;
+    }
+    public Message updateMessageWithId(Message m, int id) {
+        return null;
+    }
+    public List<Message> getMessagesFromAccount(int id) {
+        return null;
+    }
 }
